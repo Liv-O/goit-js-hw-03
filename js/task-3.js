8a32720542f986ef826c5894de2c103b5a16f6a1
Olivia <olya.nota@gmail.com>
2025-04-18 18:13:51 +0200

    first edition

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,13 @@
-function checkForSpam(message) {
-	const lowMessage = message.toLowerCase();
-	return lowMessage.indexOf('spam') >= 0 || lowMessage.indexOf('sale') >= 0;
+function filterArray(numbers, value) {
+	let newArray = [];
+	for (let el of numbers) {
+		if (el > value) newArray.push(el);
+	}
+	return newArray;
 }
 
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
